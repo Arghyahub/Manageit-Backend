11d2010d8836f2a1d9778ae16328b3adb775da08
Mayank <mayankbansal125@gmail.com>
2023-10-17 20:37:19 +0530

    Create project routes

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -2,18 +2,23 @@ import { Router, Request, Response } from "express";
 import User from "../db/User";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { IUser } from "../types";
 
 const router = Router();
 const secret = process.env.SECRET || "";
 
 // Todo: Signup route for organization/owner
 
+// For setting req.user as user, otherwise ts shows error as it can of any type
+interface CustomRequest extends Request {
+    user?: IUser;
+}
 
 // Basic login route for admin/user
-router.route("/login").post(async (req: Request, res: Response) => {
+router.route("/login").post(async (req: CustomRequest, res: Response) => {
     const { email, passwd } = req.body;
     if (!email || !passwd) {
-        return res.status(401).json({ msg: "Email or password missing", token: null, role: null });
+        return res.status(400).json({ msg: "Email or password missing", token: null });
     }
 
     try {
@@ -26,13 +31,13 @@ router.route("/login").post(async (req: Request, res: Response) => {
         if (!passValid) {
             return res.status(401).json({ msg: "Incorrect password", token: null });
         }
-
         const token = jwt.sign({ id: user._id }, secret);
+        req.user = user;
 
-        return res.status(200).json({ msg: "Login successful", token: token, role: user.role });
+        return res.status(200).json({ msg: "Login successful", token: token, user });
     } catch (err) {
         console.error("Error:", err);
-        return res.status(500).json({ msg: "Internal server error", token: null, role: null });
+        return res.status(500).json({ msg: "Internal server error", token: null });
     }
 });
 
--- src/routes/project-router.ts
+++ src/routes/project-router.ts
@@ -1,11 +1,49 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response } from 'express';
+import Project from '../db/Project';
+
 const router = Router();
 
-// Todo: Get, Post, Put, Delete for /:projectId route
+// Todo: Make a middleware to check user is part of project to allow Get request and for Post, Put, Delete route check if user has admin role
+// Todo: Implement pagination for get request for users and tasks array
 
-// Testing Route
-router.route("/").get((req: Request, res: Response) => {
-    res.send("Hello from Project Router");
-})
+router.route("/:projectId")
+    .get(async (req: Request, res: Response) => {
+        const id = req.params.projectId;
+        try {
+            const project = await Project.findById(id);
+            res.status(200).json({ "msg": "Successfully fetched the info!", "project": project });
+        } catch (error) {
+            console.error("Error in finding out:", error);
+            res.status(404).json({ "msg": "Project not found" });
+        }
+    })
+    .put(async (req: Request, res: Response) => {
+        const id = req.params.projectId;
+        try {
+            const updated = await Project.findByIdAndUpdate(id, req.body, { new: true })
+            if (updated) {
+                return res.status(200).json({ "msg": "Updated the project!" });
+            } else {
+                return res.status(404).json({ "msg": "Project not found!" });
+            }
+        } catch (error) {
+            console.log("Error while updating!", error);
+            return res.status(500).json({ "msg": "Internal Server Error!" });
+        }
+    })
+    .delete(async (req: Request, res: Response) => {
+        const id = req.params.projectId;
+        try {
+            const deletedProject = await Project.findByIdAndDelete(id);
+            if (deletedProject) {
+                res.status(200).json({ "msg": "Successfully deleted!" });
+            } else {
+                res.status(404).json({ "msg": "Project not found" });
+            }
+        } catch (error) {
+            console.log("Error while deleting! : ", error);
+            res.status(500).json({ "msg": "Internal Server Error!" });
+        }
+    });
 
 export default router;
